d7f1cdddeb65d84ca94bb7d676b347d0ab91fd2b
Tushar Negi <154955605+tusharn3115@users.noreply.github.com>
2024-12-24 13:19:06 +0000

    infinite moving card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,17 +16,24 @@ export default {
   theme: {
     extend: {
       animation: {
-        spotlight: 'spotlight 2s ease .75s 1 forwards'
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
         spotlight: {
           "0%": {
-            opacity: '0',
+            opacity: "0",
             transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
-            opacity: '1',
+            opacity: "1",
             transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
@@ -36,9 +43,7 @@ export default {
       },
     },
   },
-  plugins: [
-    addVariablesForColors,
-  ],
+  plugins: [addVariablesForColors],
 } satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
@@ -46,7 +51,7 @@ function addVariablesForColors({ addBase, theme }: any) {
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
- 
+
   addBase({
     ":root": newVars,
   });
